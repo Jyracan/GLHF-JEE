cbd6d5f0255f52d5d52b9075eac2c057e8b0c4ba
Jyracan <cmoineau@enssat.fr>
2019-03-04 17:28:05 +0100

    Essaie de créeation d' une servlet

--- src/org/connexion/ConnexionServlet.java
+++ src/org/connexion/ConnexionServlet.java
@@ -13,6 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 public class ConnexionServlet extends HttpServlet{
 	
 	private void doProcess(HttpServletRequest request, HttpServletResponse response) {
+		response.setContentType("text/html");
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			out.println("<html>");
+			out.println("<head><title>Hello World</title></head>");
+			out.println("<body>");
+			out.println("<h1>Hello World !</h1>");
+			out.println("</body></html>");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		//BookService bookService = new BookServiceImpl();
 		
 		//List<Book> listBooks =  bookService.getAllBooks();
