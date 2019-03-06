1839976cf2d7c2c8468df616032e84d27b2a342f
jyra <cmoineau@enssat.fr>
2019-03-06 23:12:26 +0100

    Ajout de script sql pour créer les tables

---
+++ DataBase/createTable.sql
@@ -0,0 +1,134 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema Etudiant
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Etudiant
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Etudiant` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema Authentification
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Authentification
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Authentification` ;
+USE `Etudiant` ;
+
+-- -----------------------------------------------------
+-- Table `Etudiant`.`Étudiant`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Etudiant`.`Étudiant` (
+  `id` VARCHAR(9) NOT NULL,
+  `sexe` VARCHAR(1) NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45) NOT NULL,
+  `dateNaissance` DATE NULL,
+  `serieBac` VARCHAR(10) NULL,
+  `anneeBac` INT NULL,
+  `mentionBac` VARCHAR(2) NULL,
+  `diplome` VARCHAR(45) NULL,
+  `anneeDiplome` INT NULL,
+  `villeDiplome` VARCHAR(45) NULL,
+  `inscription` INT NULL,
+  `courrielPro` VARCHAR(45) NULL,
+  `courrielPerso` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+COMMENT = 'use';
+
+
+-- -----------------------------------------------------
+-- Table `Etudiant`.`Groupe`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Etudiant`.`Groupe` (
+  `idGroupe` INT NOT NULL,
+  `nomGroupe` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idGroupe`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Etudiant`.`Étudiant_has_Groupe`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Etudiant`.`Étudiant_has_Groupe` (
+  `Étudiant_id` VARCHAR(9) NOT NULL,
+  `Groupe_idGroupe` INT NOT NULL,
+  PRIMARY KEY (`Étudiant_id`, `Groupe_idGroupe`),
+  INDEX `fk_Étudiant_has_Groupe_Groupe1_idx` (`Groupe_idGroupe` ASC),
+  INDEX `fk_Étudiant_has_Groupe_Étudiant_idx` (`Étudiant_id` ASC),
+  CONSTRAINT `fk_Étudiant_has_Groupe_Étudiant`
+    FOREIGN KEY (`Étudiant_id`)
+    REFERENCES `Etudiant`.`Étudiant` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Étudiant_has_Groupe_Groupe1`
+    FOREIGN KEY (`Groupe_idGroupe`)
+    REFERENCES `Etudiant`.`Groupe` (`idGroupe`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Etudiant`.`Groupe_has_Groupe`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Etudiant`.`Groupe_has_Groupe` (
+  `idGroupeAscendant` INT NOT NULL,
+  `idGroupeDescendant` INT NOT NULL,
+  PRIMARY KEY (`idGroupeAscendant`, `idGroupeDescendant`),
+  INDEX `fk_Groupe_has_Groupe_Groupe2_idx` (`idGroupeDescendant` ASC),
+  INDEX `fk_Groupe_has_Groupe_Groupe1_idx` (`idGroupeAscendant` ASC),
+  CONSTRAINT `fk_Groupe_has_Groupe_Groupe1`
+    FOREIGN KEY (`idGroupeAscendant`)
+    REFERENCES `Etudiant`.`Groupe` (`idGroupe`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Groupe_has_Groupe_Groupe2`
+    FOREIGN KEY (`idGroupeDescendant`)
+    REFERENCES `Etudiant`.`Groupe` (`idGroupe`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `Authentification` ;
+
+-- -----------------------------------------------------
+-- Table `Authentification`.`Utilisateur`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Authentification`.`Utilisateur` (
+  `id` VARCHAR(10) NOT NULL,
+  `mdp` VARCHAR(8) NOT NULL,
+  `qSecrete` VARCHAR(45) NOT NULL,
+  `rqSecrete` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Authentification`.`Droits`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Authentification`.`Droits` (
+  `Droit` INT NOT NULL,
+  `Utilisateur_id` INT NOT NULL,
+  PRIMARY KEY (`Droit`),
+  INDEX `fk_Droits_Utilisateur_idx` (`Utilisateur_id` ASC),
+  CONSTRAINT `fk_Droits_Utilisateur`
+    FOREIGN KEY (`Utilisateur_id`)
+
+    REFERENCES `Authentification`.`Utilisateur` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
