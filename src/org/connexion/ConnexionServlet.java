a80f250fb1d968ee164ca659bd19be6817d1b6b1
jyra <cmoineau@enssat.fr>
2019-03-06 21:17:37 +0100

    Création d'une page de connexion Ajout d'un script sql pour remplire la table Étudiant

--- src/org/connexion/ConnexionServlet.java
+++ src/org/connexion/ConnexionServlet.java
@@ -1,8 +1,6 @@
 package org.connexion;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,39 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ConnexionServlet extends HttpServlet{
+public class ConnexionServlet extends HttpServlet {
 	
-	private void doProcess(HttpServletRequest request, HttpServletResponse response) {
-		response.setContentType("text/html");
-		PrintWriter out;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	private void doProcess (HttpServletRequest request, HttpServletResponse response) {
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/connexion.jsp");
 		try {
-			out = response.getWriter();
-			out.println("<html>");
-			out.println("<head><title>Hello World</title></head>");
-			out.println("<body>");
-			out.println("<h1>Hello World !</h1>");
-			out.println("</body></html>");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			rd.forward(request, response);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
 		}
-		//BookService bookService = new BookServiceImpl();
-		
-		//List<Book> listBooks =  bookService.getAllBooks();
-		
-		String pageName="/accueil.jsp";
-		RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
-		//request.setAttribute("listBooks", listBooks);
-		try{rd.forward(request, response);}
-		catch(ServletException e) {e.printStackTrace();}
-		catch(IOException e) {e.printStackTrace();}
-	}
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.doProcess(req, resp);
-	}
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.doProcess(req, resp);
 	}
 }
