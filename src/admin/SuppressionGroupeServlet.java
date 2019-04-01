f2d57e9269bc64fad8fba05f83b81c22e367d34a
RomaneCastel <romane.castel@outlook.fr>
2019-04-01 18:28:51 +0200

    suppression et creation de groupe en mode admin TOTALEMENT gérés

--- src/admin/SuppressionGroupeServlet.java
+++ src/admin/SuppressionGroupeServlet.java
@@ -1,4 +1,5 @@
 package admin;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,17 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import DataAccessObjects.DBManager;
-import DataAccessObjects.DBManagerAuth;
-import DataAccessObjects.Etudiant;
 import sessionManagement.SessionVerifier;
 
-public class CreationGroupeServlet extends HttpServlet {
+public class SuppressionGroupeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (SessionVerifier.getInstance().verify(request, response)) {
 			return;
 		}
 		
-		RequestDispatcher rd = getServletContext().getRequestDispatcher("/admin/creationGroupe.jsp");
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/admin/suppressionGroupe.jsp");
 		
 		try {
 			rd.forward(request, response);
@@ -38,15 +37,19 @@ public class CreationGroupeServlet extends HttpServlet {
 		Connection connection = DBManager.getInstance().getConnection();
 		RequestDispatcher rd = getServletContext().getRequestDispatcher("/admin/gestionGroupe.jsp");
 		boolean test = false;
+		String id_group = null;
 		try {
         Statement statementGroupe = connection.createStatement();
-        ResultSet rs = statementGroupe.executeQuery("SELECT nomGroupe FROM Groupe");
+        ResultSet rs = statementGroupe.executeQuery("SELECT idGroupe, nomGroupe FROM Groupe");
         while(rs.next())
         {
-         // Si le nom du groupe existe déjà, on l'insère pas dans la bdd
-      	  if(rs.getString("nomGroupe").contentEquals(groupName)) {
-      		test = true;
-      		RequestDispatcher rd2 = getServletContext().getRequestDispatcher("/admin/creationGroupe.jsp");
+         // Si le nom du groupe n'est pas dans la bdd, on ne le supprime pas
+        	if(rs.getString("nomGroupe").contentEquals(groupName)) {
+        		test = true;
+        	}
+         }
+        if(!test) {
+      		RequestDispatcher rd2 = getServletContext().getRequestDispatcher("/admin/gestionGroupe.jsp");
     		
     		try {
     			rd2.forward(request, response);
@@ -56,30 +59,41 @@ public class CreationGroupeServlet extends HttpServlet {
     			e.printStackTrace();
     		}
       	  }
-         }
+        else {
+	        Statement statementGroupe2 = connection.createStatement();
+	        ResultSet rs3 = statementGroupe.executeQuery("SELECT idGroupe FROM Groupe where nomGroupe =\""+groupName +"\";");
+	        id_group=rs3.getString("idGroupe");
+        }
 		}
 		catch (SQLException e) 
         {
             e.printStackTrace();
         }
 		
-        try {
-        	if(!groupName.isEmpty() && !test) {
+		
+		try {
+			if(!groupName.isEmpty() && !test) {
 	        	Statement statement = connection.createStatement();
 	        	String id = (String) request.getSession().getAttribute("login");
-	        	String query = "insert into Groupe(nomGroupe, redacteur) values (" + "\""+groupName +"\""+ ",\""+ id+"\");";
+	        	String query = "delete from Groupe where nomGroupe = \"" + groupName +"\";";
+	        	String queryStudent = " delete from Etudiant_has_Groupe where Groupe_idGroupe =\""+id_group+"\";";
+	        	String queryGroup = " delete from Groupe_has_Groupe where idGroupeAscendant =\""+id_group+"\" or idGroupeDescendant =\""+id_group+"\";";
 	        	System.out.println(query);
+	        	System.out.println(queryStudent);
+	        	System.out.println(queryGroup);
 	        	int rs2 = statement.executeUpdate(query);
-        	}
+	        	int rs3 = statement.executeUpdate(queryStudent);
+	        	int rs4 = statement.executeUpdate(queryGroup);
+			}
         	
         }
         catch (SQLException e) 
         {
             e.printStackTrace();
         }
-        
-        
-        //Redirection
+		
+		
+		//Redirection
         
         RequestDispatcher rdd = getServletContext().getRequestDispatcher("/admin/gestionGroupe.jsp");
 		
@@ -91,5 +105,5 @@ public class CreationGroupeServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
